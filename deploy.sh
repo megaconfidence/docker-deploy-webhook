1b394e1456c3dc3c636330350960519d678f1a7c
cokoghenun <confidenceboi@gmail.com>
2020-10-25 19:35:02 +0000

    feat: Adds support for build args

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ if $(docker images -a | grep -q $IMAGE_NAME); then
   docker rmi -f $IMAGE_NAME
   docker system prune -f
 fi
-docker build --tag $IMAGE_NAME $REPO_URL
+docker build --tag $IMAGE_NAME $(for i in `cat .env`; do out+="--build-arg $i " ; done; echo $out;out="") $REPO_URL
 if $(docker ps -a | grep -q $CONTAINER_NAME); then
   docker stop $CONTAINER_NAME
 fi
-docker run -dit --rm -p $PORT:$PORT --env-file .env --name $CONTAINER_NAME $IMAGE_NAME
+docker run -d --restart always -p $PORT:$PORT --name $CONTAINER_NAME $IMAGE_NAME
